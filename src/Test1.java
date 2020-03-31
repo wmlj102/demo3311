1329adba2a5e885b8eebba43a30e89335f653dff
李京 <lij102102@gmail.com>
2020-03-31 15:23:08 +0800

    老许上班第一天提交的代码

--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,7 @@
 public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("动次打次");
+        System.out.println("sds");
+        System.out.println("qwe");
+    }
 }
